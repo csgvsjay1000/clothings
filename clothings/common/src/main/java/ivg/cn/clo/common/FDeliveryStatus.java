97de64c122896f54581f6409c582ca1012e0724c
csgvsjay1000 <254633397@qq.com>
2019-04-26 16:23:03 +0800

    添加common

---
+++ clothings/common/src/main/java/ivg/cn/clo/common/FDeliveryStatus.java
@@ -0,0 +1,10 @@
+package ivg.cn.clo.common;
+
+/**  工厂发货单相关状态 */
+public interface FDeliveryStatus {
+
+	/**  箱状态 */
+	interface PackStatus{
+		
+	}
+}
---
+++ clothings/common/src/main/java/ivg/cn/clo/common/TagEpcStatus.java
@@ -0,0 +1,6 @@
+package ivg.cn.clo.common;
+
+/**  标签服务EPC相关状态 */
+public interface TagEpcStatus {
+
+}
